ca081c8141e5da8e2dd3d0c3291f5f76f7b6f6bd
Batuhan Berk Başoğlu <batuhan@basoglu.ca>
2021-03-28 20:42:46 -0400

    Added the slogan.

---
+++ src/homePage/homepage.js
@@ -0,0 +1,24 @@
+import { Component } from "react";
+import './Homepage.css'
+
+class Homepage extends Component {
+    render() {
+        return (
+            <div className="Homepage">
+                <section className="colored-section2" id="title">
+                    <div className="container-fluid">
+                        <div className="row">
+                            <div className="col-12">
+                                <h1 className="big-heading">YOU ARE NOT BUYING A HOUSE, </h1>
+                                <h1 className="big-heading">YOU ARE BUYING A LIFESTYLE. </h1>
+                                
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+    );
+  }
+}
+
+export default Homepage;
