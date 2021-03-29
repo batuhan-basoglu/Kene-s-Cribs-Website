27a03a4311a405c4ba41df63eeead824a8c7d93e
Arctichawk1 <batuhanbasoglu@mailbox.org>
2021-03-29 18:35:01 -0400

    Added the carousel.

--- src/homePage/homepage.js
+++ src/homePage/homepage.js
@@ -1,5 +1,9 @@
 import { Component } from "react";
-import './Homepage.css'
+import './Homepage.css';
+import house1 from '../images/house1.jpg';
+import house2 from '../images/house2.jpg';
+import house3 from '../images/house3.jpg';
+import Carousel from 'react-bootstrap/Carousel'
 
 class Homepage extends Component {
     render() {
@@ -11,14 +15,42 @@ class Homepage extends Component {
                             <div className="col-12">
                                 <h1 className="big-heading">YOU ARE NOT BUYING A HOUSE, </h1>
                                 <h1 className="big-heading">YOU ARE BUYING A LIFESTYLE. </h1>
-                                
                             </div>
                         </div>
                     </div>
                 </section>
+                <section className="white-section" id="white-section">
+                    <div className="housesIntro">
+                        <Carousel>
+                            <Carousel.Item>
+                                <img
+                                    className="headerImg"
+                                    src={house1}
+                                    alt="First House"
+                                />
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="headerImg"
+                                    src={house2}
+                                    alt="Second House"
+                                />
+
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img
+                                    className="headerImg"
+                                    src={house3}
+                                    alt="Third House"
+                                />
+
+                            </Carousel.Item>
+                        </Carousel>
+                    </div>
+                </section>
             </div>
-    );
-  }
+        );
+    }
 }
 
 export default Homepage;
