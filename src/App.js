47bdf96a3397f954c70f76b9583b7d013de1855b
Arctichawk1 <batuhanbasoglu@mailbox.org>
2021-03-30 06:20:59 -0400

    Re-added the files.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
 import { Component } from "react";
+import Homepage from './homePage/Homepage';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1>Kene's Cribs</h1>
-          <p>Real Website loading.......</p>
-        </header>
-        <section>
-          <h1>hello everyone</h1>
-          <div class="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-          </div>
-          <div class="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-          </div>
-          <div class="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-          </div>
-          <div class="alert alert-primary" role="alert">
-            A simple primary alert—check it out!
-          </div>
-        </section>
+          <Homepage></Homepage>
       </div>
     );
   }
