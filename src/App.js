a4b12548feda13e4d07a250312293c3369568902
kene17 <keneojukwu17@gmail.com>
2021-03-30 19:50:09 -0400

    added agents page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Component } from "react";
-import Homepage from './homePage/Homepage'
+/* import Homepage from './homePage/Homepage' */
+import AgentPage from './agent-page/AgentPage'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Homepage/>
+        {/* <Homepage/> */}
+        <AgentPage/>
       </div>
     );
   }
