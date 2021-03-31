7de6ee1588f115cae91c057d22f41e3ab2913817
kene17 <keneojukwu17@gmail.com>
2021-03-31 14:10:35 -0400

    Updated Agent Page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Component } from "react";
 import AgentPage from './agent-page/AgentPage'
 
 
-import Homepage from './homePage/Homepage';
+// import Homepage from './homePage/Homepage';
 
 class App extends Component {
   render() {
@@ -11,7 +11,7 @@ class App extends Component {
       <div className="App">
         {/* <Homepage/> */}
         <AgentPage/>
-        <Homepage/>
+        {/* <Homepage/> */}
         
       </div>
     );
