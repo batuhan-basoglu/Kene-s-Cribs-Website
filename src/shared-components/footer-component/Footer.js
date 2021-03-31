b03cbe3164c558bd0e190d3de0d78d867d186755
soyed <oyediransamuel1995@gmail.com>
2021-03-31 00:30:37 -0400

    made the route for the pages, navbar and footer of the application

---
+++ src/shared-components/footer-component/Footer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Footer.css";
+const Footer = () => {
+  return (
+    <footer>
+      <section id="footer">
+        <div class="container-fluid">
+          <i class="fab fa-twitter social-icon"></i>
+          <i class="fab fa-facebook social-icon"></i>
+          <i class="fab fa-instagram social-icon"></i>
+          <i class="fas fa-envelope social-icon"></i>
+          <p class="">© Copyright SEG3125 - Group 3</p>
+        </div>
+      </section>
+    </footer>
+  );
+};
+export default Footer;
