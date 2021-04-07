a9b5a9e70244657c7b08049d2cffbb5f4c0317a7
ruchi <ruchirawperera@gmail.com>
2021-04-07 16:38:34 -0400

    filter, search bar

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Footer from "./shared-components/footer-component/Footer";
 import Navbar from "./shared-components/navbar-component/Navbar";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import i18next from "i18next";
-import { useTranslation } from "react-i18next";
 import Cookies from "js-cookie";
 import HomePage from "./homePage/Homepage";
 import ListingsPage from "./listings-page/ListingsPage";
@@ -31,7 +30,6 @@ function App() {
   const currentLanguage = langauges.find(
     (lang) => lang.code === currentLanguageCode
   );
-  const { t } = useTranslation();
 
   useEffect(() => {
     document.body.dir = currentLanguage.dir || "ltr";
