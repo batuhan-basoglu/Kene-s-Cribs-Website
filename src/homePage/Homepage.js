37a3ada47ce882f79fb9527ccb2b88d1f06b65c3
Batuhan Berk Başoğlu <batuhan@basoglu.ca>
2021-04-07 15:35:59 -0400

    Made the translation for the homepage and contact us page.

--- src/homePage/Homepage.js
+++ src/homePage/Homepage.js
@@ -6,6 +6,7 @@ import house3 from "../images/house3.jpg";
 import house4 from "../images/house4.jpg";
 import Carousel from "react-bootstrap/Carousel";
 import Button from "react-bootstrap/Button";
+import { withTranslation } from "react-i18next";
 
 
 
@@ -19,6 +20,7 @@ class Homepage extends Component {
   }
 
   render() {
+    const { t } = this.props;
     const { nextIcon, prevIcon } = this.state;
     return (
       <div className="homePage">
@@ -26,8 +28,8 @@ class Homepage extends Component {
           <div className="container-fluid">
             <div className="row">
               <div className="col-12">
-                <h1 className="big-heading">YOU ARE NOT BUYING A HOUSE, </h1>
-                <h1 className="big-heading">YOU ARE BUYING A LIFESTYLE. </h1>
+                <h1 className="big-heading">{t("Slogan_1")}</h1>
+                <h1 className="big-heading">{t("Slogan_2")}</h1>
               </div>
             </div>
           </div>
@@ -39,9 +41,9 @@ class Homepage extends Component {
                 <img className="headerImg" src={house1} alt="First House" />
                 <Carousel.Caption>
                   <h3 style={{ fontSize: "250%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>100 Charlie Rogers, Kanata, ON K2V 1A7</h3>
-                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>1 Bedroom, 2 Bathroom</h2>
+                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>{t("Rooms")}</h2>
                   <div className="buttonka">
-                    <Button href="/listings" variant="info">Click for Details</Button>
+                    <Button href="/listings" variant="info">{t("Info")}</Button>
                   </div>
                 </Carousel.Caption>
               </Carousel.Item>
@@ -49,9 +51,9 @@ class Homepage extends Component {
                 <img className="headerImg" src={house2} alt="Second House" />
                 <Carousel.Caption>
                   <h3 style={{ fontSize: "250%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>1490 Youville Drive, Orléans, ON K1C 2X8</h3>
-                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>1 Bedroom, 2 Bathroom</h2>
+                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>{t("Rooms")}</h2>
                   <div className="buttonka">
-                    <Button href="/listings" variant="info">Click for Details</Button>
+                    <Button href="/listings" variant="info">{t("Info")}</Button>
                   </div>
                 </Carousel.Caption>
               </Carousel.Item>
@@ -59,9 +61,9 @@ class Homepage extends Component {
                 <img className="headerImg" src={house3} alt="Third House" />
                 <Carousel.Caption>
                   <h3 style={{ fontSize: "250%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>8720 Russell Road, Navan, ON K4B 1J1</h3>
-                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>1 Bedroom, 2 Bathroom</h2>
+                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>{t("Rooms")}</h2>
                   <div className="buttonka">
-                    <Button href="/listings" variant="info">Click for Details</Button>
+                    <Button href="/listings" variant="info">{t("Info")}</Button>
                   </div>
                 </Carousel.Caption>
               </Carousel.Item>
@@ -69,9 +71,9 @@ class Homepage extends Component {
                 <img className="headerImg" src={house4} alt="Fourth House" />
                 <Carousel.Caption>
                   <h3 style={{ fontSize: "250%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>2785 8th Line Road, Metcalfe, ON K0A 2P0</h3>
-                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>1 Bedroom, 2 Bathroom</h2>
+                  <h2 style={{ fontSize: "150%", textShadow: "-2px 0 black, 0 2px black, 2px 0 black, 0 -2px black" }}>{t("Rooms")}</h2>
                   <div className="buttonka">
-                    <Button href="/listings" variant="info">Click for Details</Button>
+                    <Button href="/listings" variant="info">{t("Info")}</Button>
                   </div>
                 </Carousel.Caption>
               </Carousel.Item>
@@ -80,13 +82,13 @@ class Homepage extends Component {
               <div className="container-fluid">
                 <div className="row">
                   <div className="col-12">
-                    <h2 style={{ paddingLeft: "22rem", paddingBottom: "1rem", fontFamily: "Trebuchet MS" }}>Description </h2>
-                    <p style={{ fontSize: "110%" }}> Welcome to Kene's Cribs. Kene's Cribs is a real estator company which provides the clients with the houses. <br></br>
-                  	 The houses limited to the clients are only limited to the clients' dreams. In order to further navigate the<br></br>
-                     website, use the navigation bar to switch between pages. Listings page is for browsing houses, Agents page<br></br>
-                     is for browsing the estators, the Contact Us page is for the contacting us and the login page is for our<br></br>
-                     members who want to benefit from our deals. Above are some houses which can interest you. Feel free to<br></br>
-                     click the buttons above to explore the houses you want. </p>
+                    <h2 style={{ paddingLeft: "22rem", paddingBottom: "1rem", fontFamily: "Trebuchet MS" }}>{t("Desc")}</h2>
+                    <p style={{ fontSize: "110%" }}>{t("Home1")}<br></br>
+                    {t("Home2")}<br></br>
+                    {t("Home3")}<br></br>
+                    {t("Home4")}<br></br>
+                    {t("Home5")}<br></br>
+                    {t("Home6")}</p>
                   </div>
                 </div>
               </div>
@@ -98,4 +100,4 @@ class Homepage extends Component {
   }
 }
 
-export default Homepage;
+export default withTranslation()(Homepage);
